551a6512488131777b7dc818dc5ce974d11561c9
Doug Wells <dougwells@users.noreply.github.com>
2016-06-18 16:48:54 -0700

    Added app test of TodoApp

--- app/tests/app.test.jsx
+++ app/tests/app.test.jsx
@@ -1,7 +1,28 @@
 var expect = require('expect');
+var React = require('react');
+var ReactDOM = require('react-dom');
+var TestUtils = require('react-addons-test-utils');
+var $ = require('jQuery');
+
+var TodoApp = require('TodoApp');
+var todoData = [{
+		id: 11,
+		text: "Test Features",
+		completed: false
+	}]
 
 describe('App', () => {
   it('should properly run tests', () => {
     expect(1).toBe(1);
   });
+  
+  
+  it('should toggle change value when handleToggle called', () => {
+    var todoApp = TestUtils.renderIntoDocument(<TodoApp />);
+    todoApp.setState({todos: todoData});
+    expect(todoApp.state.todos[0].completed).toBe('false');
+    TestUtils.Simulate.todoApp.handleToggle(11);
+    expect(todoApp.state.todos[0].completed).toBe('true');
+  });
+  
 });
