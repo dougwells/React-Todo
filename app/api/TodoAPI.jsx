ff0c744f92c04c467471fc0733540348d30df675
dougwells <dougwells@gmail.com>
2016-06-19 09:17:58 -0700

    Testing TodoAPI, part 1

---
+++ app/api/TodoAPI.jsx
@@ -0,0 +1,46 @@
+var $ = require('jquery');
+var uuid = require('node-uuid');
+
+module.exports = {
+  setTodos: function(todos){
+    if($.isArray(todos)){
+      localStorage.setItem('todos', JSON.stringify(todos));
+      return(todos);
+    }
+  },
+
+  getTodos: function(){
+    var stringTodos = localStorage.getItem('todos');
+    var todos = [];
+    try {
+      var todos = JSON.parse(stringTodos);
+    } catch(err) {
+      console.log("Error: getTodos could not updating todos", err);
+    }
+
+    return $.isArray(todos) ? todos : [];
+  }
+}
+
+var startArr = [
+  {
+    id: uuid(),
+    text: "Mow lawn",
+    completed: false
+  },
+  {
+    id: uuid(),
+    text: "Grocery shop",
+    completed: true
+  },
+  {
+    id: uuid(),
+    text: "Clean up house",
+    completed: false
+  },
+  {
+    id: uuid(),
+    text: "Take out trash",
+    completed: false
+  }
+];
---
+++ app/tests/api/TodoAPI.test.jsx
@@ -0,0 +1,28 @@
+var expect = require('expect');
+var TodoAPI = require('TodoAPI');
+
+
+describe('TodoAPI', () => {
+    it('should exist', () => {
+      expect(TodoAPI).toExist();
+    });
+
+
+  describe('setTodos', ()=>{
+    it('should set valid todos array', ()=>{
+    var todos = [{
+      id: 23,
+      text: "test",
+      completed: false
+    }];
+    TodoAPI.setTodos(todos);
+    var actualTodos = JSON.parse(localStorage.getItem('todos'));
+    expect(actualTodos).toEqual(todos);
+    });
+  });
+
+  describe('getTodos', ()=>{
+
+  });
+
+});
