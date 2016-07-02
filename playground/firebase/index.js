feaee75020ba41cf96ee51ced3794e34c820ab35
dougwells <dougwells@gmail.com>
2016-07-02 16:45:54 -0600

    Firebase: Deleting & Reading Data

--- playground/firebase/index.js
+++ playground/firebase/index.js
@@ -10,25 +10,41 @@ var config = {
 firebase.initializeApp(config);
 var firebaseRef = firebase.database().ref();
 firebaseRef.set({
+  developer: 'Doug Wells',
   app: {
     name: "React Todo App",
-    version: 1.2
+    version: '1.2.11'
   },
 
   user: {
-    name: "Doug Wells",
+    name: "Ted",
     age: 52,
     isOld: false
 }
-}).then(()=>{
-  console.log("Success.  Set to firebase worked as expected")
-}, (e)=>{
-  console.dir('Error in call to Firebase from playground/firebase/index.js', e);
-}
-);
+});
 
-firebaseRef.child('user').set({
-  name: "Doug W.",
-  isOld: false,
-  age: 52
-})
+// firebaseRef.child('user').once('value').then(
+//   (snapshot)=> {console.log('FB Data Retrieved', snapshot.val())},
+//   (err)=> {console.log("Error retrieiving FB data.  Error: ", err)}
+// );
+
+firebaseRef.child('user').on('value', (snapshot)=>{
+  console.log('We Have Your Data: ', snapshot.val());
+});
+firebaseRef.update({'user/name': 'Erik'});
+firebaseRef.update({'app/name': 'Modern Todo App'});
+// firebaseRef.off();
+
+
+// firebaseRef.update({
+//   "app/name": "Todo App with React-Redux-Firebase ",
+//   "user/name" : "Erik"
+// });
+
+// firebaseRef.child('app').update({name: 'Todo App'});
+// firebaseRef.off();
+// firebaseRef.child('user').update({name:'Erk', isOld: true});
+
+// firebaseRef.child('app').update({name: null, version: '2.0.0'});
+// firebaseRef.child('user').update({age: null});
+// firebaseRef.child('developer').remove();
