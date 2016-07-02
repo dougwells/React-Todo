fd6b474419ce4932639634a0e46b5252b3482b85
dougwells <dougwells@gmail.com>
2016-07-02 12:02:53 -0600

    Firebase - using set({...}) to save data

--- playground/firebase/index.js
+++ playground/firebase/index.js
@@ -8,6 +8,27 @@ var config = {
   storageBucket: "react-todo-app-udemy-mead.appspot.com",
 };
 firebase.initializeApp(config);
-firebase.database().ref().set({
-  appName: 'React Todo App'
-});
+var firebaseRef = firebase.database().ref();
+firebaseRef.set({
+  app: {
+    name: "React Todo App",
+    version: 1.2
+  },
+
+  user: {
+    name: "Doug Wells",
+    age: 52,
+    isOld: false
+}
+}).then(()=>{
+  console.log("Success.  Set to firebase worked as expected")
+}, (e)=>{
+  console.dir('Error in call to Firebase from playground/firebase/index.js', e);
+}
+);
+
+firebaseRef.child('user').set({
+  name: "Doug W.",
+  isOld: false,
+  age: 52
+})
